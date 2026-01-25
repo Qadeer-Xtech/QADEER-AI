c08ef3f2cf7cb0fff242662e26d0c49992cc2b10
Mr.ABBAS <apnaqadeer007@gmail.com>
2026-01-25 08:40:35 +0500

    ᴄʀᴇᴅɪᴛ: 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 [Dev]

---
+++ clintplugins/Editting/trash.js
@@ -0,0 +1,34 @@
+const canvacord = require("canvacord");
+
+module.exports = async (context) => {
+        const { client, m, Tag, botname } = context;
+
+let cap = `Converted By ${botname}`;
+
+try {
+
+        if (m.quoted) {
+            try {
+                img = await client.profilePictureUrl(m.quoted.sender, 'image')
+            } catch {
+                img = "https://i.ibb.co/5hrtK2Lv/20251009-062614.jpg"
+            }
+                        result = await canvacord.Canvacord.trash(img);
+        } else if (Tag) {
+            try {
+                ppuser = await client.profilePictureUrl(Tag[0] || m.sender, 'image')
+            } catch {
+                ppuser = 'https://i.ibb.co/5hrtK2Lv/20251009-062614.jpg'
+            }
+                        result = await canvacord.Canvacord.trash(ppuser);
+        } 
+
+
+        await client.sendMessage(m.chat, { image: result, caption: cap }, { quoted: m });
+
+} catch (e) {
+
+m.reply("Something wrong occured. 😞")  
+
+}
+    }
---
+++ clintplugins/Editting/trigger.js
@@ -0,0 +1,47 @@
+const { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter')
+
+const canvacord = require("canvacord");
+
+module.exports = async (context) => {
+        const { client, m, Tag, botname } = context;
+
+let cap = `Converted By ${botname}`;
+
+try {
+
+        if (m.quoted) {
+            try {
+                img = await client.profilePictureUrl(m.quoted.sender, 'image')
+            } catch {
+                img = "https://i.ibb.co/5hrtK2Lv/20251009-062614.jpg"
+            }
+                        result = await canvacord.Canvacord.trigger(img);
+        } else if (Tag) {
+            try {
+                ppuser = await client.profilePictureUrl(Tag[0] || m.sender, 'image')
+            } catch {
+                ppuser = 'https://i.ibb.co/5hrtK2Lv/20251009-062614.jpg'
+            }
+                        result = await canvacord.Canvacord.trigger(ppuser);
+        } 
+
+        let sticker = new Sticker(result, {
+            pack: `dreaded`,
+            author:"" ,
+            categories: ['🤩', '🎉'],
+            id: '12345',
+            quality: 75,
+            background: 'transparent' // The sticker background color (only for full stickers)
+        })
+        const stikk = await sticker.toBuffer()
+       await client.sendMessage(m.chat, {sticker: stikk}, {quoted: m})
+
+
+        
+
+} catch (e) {
+
+m.reply("Something wrong occured. 😞")  
+
+}
+    }
