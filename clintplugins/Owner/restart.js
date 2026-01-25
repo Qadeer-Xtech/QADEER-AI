a317d8864db94d6c4e15d6e625be62450f3ca9f4
Mr.ABBAS <apnaqadeer007@gmail.com>
2026-01-25 08:29:15 +0500

    ᴄʀᴇᴅɪᴛ: 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 [Dev]

---
+++ clintplugins/Owner/restart.js
@@ -0,0 +1,15 @@
+const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware');
+
+module.exports = async (context) => {
+    await ownerMiddleware(context, async () => {
+        const { client, m } = context;
+        
+        await client.sendMessage(m.chat, { react: { text: '🔄', key: m.key } });
+        await m.reply("Restarting QADEER-AI");
+        
+        const { exec } = require('child_process');
+        exec('heroku restart', () => {});
+        
+        setTimeout(() => { process.exit(0); }, 3000);
+    });
+};
